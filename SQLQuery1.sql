543889f45aceb718e2effa18010977cdf8b178a6
ingmarkroll79 <146067161+ingmarkroll79@users.noreply.github.com>
2023-10-02 21:39:46 +0200

    Update SQLQuery1.sql
    Comments added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,7 +1,7 @@
+/* No longer needed Selects for data preparation 
 select Entity, Year, [ DALYs_from_depressive_disorders_per_100K_people]
 from SQL_Projekte..mental_health
 where entity = 'Germany'
-
 
 select entity, Year, [Number of Internet users] from SQL_Projekte..number_of_internet_users
 where entity = 'Germany' and
@@ -10,8 +10,9 @@ where entity = 'Germany' and
 select entity, Year, Population from SQL_Projekte..Population_Germany
 where entity = 'Germany' and
       Year <= '2019'
+*/
 
-
+-- We want to get the number of internet users and the number of depressive disorders in Germany  
 SELECT
     t1.Entity,
     t1.Year,
